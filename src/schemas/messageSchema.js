094a402f26c25f1902d87534c0076462cdae0f7c
Limbureuben <limbureuben4@gmail.com>
2025-01-18 01:08:07 +0300

    The changes

---
+++ src/schemas/messageSchema.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server-express');
+
+const messageSchema = gql`
+    type Message {
+        id: ID!
+        email: String!
+        message: String!
+        data: String!
+        createdAt: String!
+    }
+    
+    input SendMessageInput {
+    email: String!
+    message: String!
+    data: String!
+  }
+
+  type Query {
+    messagesByEmail(email: String!): [Message]
+    allMessages: [Message]
+  }
+
+  type Mutation {
+    sendMessage(input: SendMessageInput!): Message
+  }
+
+`;
+
+module.exports = messageSchema;
